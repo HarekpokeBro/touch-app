675a56256efdb609320478de072128b8a49cdc14
HarekpokeBro <soerenbjerg@gmail.com>
2017-01-21 16:21:47 +0100

    3
    3

--- touchapp.js
+++ touchapp.js
@@ -2,3 +2,8 @@ function init(){
 	var touchzone=document.getElementById("touchzone");
 	touchzone.addEventListener("touchzone",touchHandler,false);
 }
+
+function touchHandler(event){
+	var coords=document.getElementById("coords");
+	coords.innerHTML = 'x: ' + event.touches[0].pageX + ', y: ' + event.touches[0].pageY;
+}
